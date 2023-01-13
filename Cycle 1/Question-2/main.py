d56bd877df4347e5d5487e6e290888ac68fea824
N44 <91179000+nandhu-44@users.noreply.github.com>
2023-01-13 06:14:08 +0000

    Adding the comments

--- Cycle 1/Question-2/main.py
+++ Cycle 1/Question-2/main.py
@@ -1,3 +1,10 @@
+"""
+2. Develop a program to read the three sides of two triangles and calculate the area of both.
+Define a function to read the three sides and call it.
+Also, define a function to calculate the area. 
+Print the total area enclosed by both triangles and each triangle's contribution (%) towards it.
+"""
+
 # Importing math library to use the square root function
 import math
 
