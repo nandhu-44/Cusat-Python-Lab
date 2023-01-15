66c493730fb1d65464e18ade99d53143bb76bcbe
nandhu-44 <nandhu90040@gmail.com>
2023-01-15 09:30:13 +0530

    Modified the user interface

--- Cycle 1/Question-2/main.py
+++ Cycle 1/Question-2/main.py
@@ -10,7 +10,8 @@ import math
 
 # Function Declarations
 # Function to take user input of the three sides
-def read_sides():
+def read_sides(triangle_number):
+    print(f">> Triangle {triangle_number} <<")
     side1 = float(input("Enter the length of side 1: "))
     side2 = float(input("Enter the length of side 2: "))
     side3 = float(input("Enter the length of side 3: "))
@@ -44,37 +45,55 @@ def is_valid_triangle(side1, side2, side3):
 
 # Main Program
 # Triangle 1
-print(">> Triangle 1 <<")
-side_a1,side_b1,side_c1 = read_sides()
+print()
+side_a1,side_b1,side_c1 = read_sides(1)
 if(is_valid_triangle(side_a1,side_b1,side_c1) == False):
-    print("Please provide the lengths of a triangle that ia valid!")
+    print("Please provide the lengths of a triangle that is valid!")
+    print()
     exit()
 area_1 = find_area(side_a1,side_b1,side_c1)
 
 # Trinagle 2
-print(">> Triangle 2 <<")
-side_a2,side_b2,side_c2 = read_sides()
+print()
+side_a2,side_b2,side_c2 = read_sides(2)
 if(is_valid_triangle(side_a2,side_b2,side_c2) == False):
-    print("Please provide the lengths of a triangle that ia valid!")
+    print("Please provide the lengths of a triangle that is valid!")
+    print()
     exit()
 area_2 = find_area(side_a2,side_b2,side_c2)
 
 
-print()
-
-# Printing the areas of the triangles
-print(f"The area of the triangle is {area_1}")
-print(f"The area of the triangle is {area_2}")
-
-print()
-
-# Finding the total area of the two triangles
-total_area_of_both = find_total_area(area_1,area_2)
-print(f"The total area of the two triangles is {total_area_of_both}")
-
-print()
-
-# Finding the percentage contribution of each triangle
-percentage_contribution1,percentage_contribution2 = percentage_contribution(area_1,area_2)
-print(f"The percentage contribution of triangle 1 is {percentage_contribution1}%")
-print(f"The percentage contribution of triangle 2 is {percentage_contribution2}%")
+yes = True
+content = """
+______ Main Menu ______
+1. Find the area of each triangle
+2. Find the total area of both triangles
+3. Find the percentage contribution of each triangle
+4. Exit
+"""
+while yes:
+    print(content)
+    choice = int(input("Enter your choice: "))
+    print()
+    print("______________________________________________________________________________________________")
+    print()
+    if choice == 1:
+        # Printing the areas of the triangles
+        print(f"The area of the triangle 1 is {round(area_1,2)} sq. units")
+        print(f"The area of the triangle 2 is {round(area_2,2)} sq. units")
+    elif choice == 2:
+        # Finding the total area of the two triangles
+        total_area_of_both = find_total_area(area_1,area_2)
+        print(f"The total area of the two triangles is {round(total_area_of_both,2)} sq. units")
+    elif choice == 3:
+        # Finding the percentage contribution of each triangle
+        percentage_contribution1,percentage_contribution2 = percentage_contribution(area_1,area_2)
+        print(f"The percentage contribution of triangle 1 is {percentage_contribution1}%")
+        print(f"The percentage contribution of triangle 2 is {percentage_contribution2}%")
+    elif choice == 4:
+        print("Thank you for using the program!")
+        yes = False
+    else:
+        print("Please enter a valid choice!")
+    print("______________________________________________________________________________________________")
+    print()
