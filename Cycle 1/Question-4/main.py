29e9b1a454351b49b5ba0e9d22696b455f103ca6
nandhu-44 <nandhu90040@gmail.com>
2023-01-15 10:16:28 +0530

    Modified the user interface

--- Cycle 1/Question-4/main.py
+++ Cycle 1/Question-4/main.py
@@ -55,28 +55,47 @@ def list_to_string(list):
     return string[:-2]
 
 # Main Program
-# Checking if a number is happy or not
-number = int(input("Enter a number: "))
-if check_happy_number(number):
-    print(f"{number} is a happy number")
-else:
-    print(f"{number} is not a happy number")
 
-print()
+yes = True
+content = """
+______ Main Menu ______
+1. Check if a number is happy or not
+2. Print all happy numbers in a range
+3. Print first N happy numbers
+4. Exit
+"""
+while yes:
+    print(content)
+    choice = int(input("Enter your choice: "))
+    print()
+    print("______________________________________________________________________________________________")
+    print()
+    if choice == 1:
+        # Checking if a number is happy or not
+        number = int(input("Enter a number: "))
+        if check_happy_number(number):
+            print(f"{number} is a happy number")
+        else:
+            print(f"{number} is not a happy number")
+    elif choice == 2:
+        # Printing all happy numbers in a range
+        lower_limit = int(input("Enter the lower limit: "))
+        upper_limit = int(input("Enter the upper limit: "))
+        happy_numbers = happy_numbers_in_range(lower_limit, upper_limit)
 
-# Printing all happy numbers in a range
-lower_limit = int(input("Enter the lower limit: "))
-upper_limit = int(input("Enter the upper limit: "))
-happy_numbers = happy_numbers_in_range(lower_limit, upper_limit)
-
-if len(happy_numbers) == 0:
-    print("There are no happy numbers in the given range!")
-else:
-    print(f"There are {len(happy_numbers)} happy numbers in the range from {lower_limit} to {upper_limit} which are: {list_to_string(happy_numbers)}")
-
-print()
-
-# Printing first N happy numbers
-n = int(input("Enter the no of terms(happy numbers) to print: "))
-happy_numbers = first_n_happy_numbers(n)
-print(f"The first {n} happy numbers are: {list_to_string(happy_numbers)}")
+        if len(happy_numbers) == 0:
+            print("There are no happy numbers in the given range!")
+        else:
+            print(f"There {'is' if len(happy_numbers) == 1 else 'are'} {len(happy_numbers)} happy number{'' if len(happy_numbers) == 1 else 's'} in the range from {lower_limit} to {upper_limit} which {'is' if len(happy_numbers) == 1 else 'are'}: {list_to_string(happy_numbers)}")
+    elif choice == 3:
+        # Printing first N happy numbers
+        n = int(input("Enter the no of terms(happy numbers) to print: "))
+        happy_numbers = first_n_happy_numbers(n)
+        print(f"The first {n} happy number{'' if len(happy_numbers) == 1 else 's'} {'is' if len(happy_numbers) == 1 else 'are'}: {list_to_string(happy_numbers)}")
+    elif choice == 4:
+        print("Thank you for using the program!")
+        yes = False
+    else:
+        print("Please enter a valid choice!")
+    print("______________________________________________________________________________________________")
+    print()
