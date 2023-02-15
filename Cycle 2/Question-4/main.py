06db36c99f595e4a68d776a4fe3a6cb84aedad0c
nandhu-44 <nandhu90040@gmail.com>
2023-02-15 18:49:25 +0530

    Comments modification

--- Cycle 2/Question-4/main.py
+++ Cycle 2/Question-4/main.py
@@ -1,3 +1,13 @@
+"""
+4. Write a program to create a class Box with data members length,
+breadth, height, area, and volume. Provider constructor that enables
+initialization with one parameter (for cube), two parameters (for
+square prism) three parameters (rectangular prism). Also, provide
+functions to calculate area and volume.
+Create a list of N boxes with random measurements and print the
+details of the box with maximum volume: area ratio*
+"""
+
 # Importing random module to generate random numbers
 import random
 
